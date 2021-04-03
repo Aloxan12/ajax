4a0e09f2c7a77f4306344ee1e3ce2e67e0a65303
Aloxan12 <kvakvaaaak@gmail.com>
2021-04-03 18:00:16 +0300

    callback

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
 
 let a = 5;
 
-$.ajax('https://repetitora.net/api/JS/Images');
-
+$.ajax('https://repetitora.net/api/JS/Images', {
+    success: function(data){
+        data.forEach(el=>{
+            const img = document.createElement('img');
+            img.src = el.thumbnail
+            document.querySelector('body').appendChild(img)
+        })
+    }
+});
 a = 8;
-
-console.log(a);
