064540fd50c546abe1ebd1876411838ac6d4c2ea
Aloxan12 <kvakvaaak@mail.ru>
2021-05-03 16:15:55 +0300

    refactor

--- main.js
+++ main.js
@@ -1,23 +1,18 @@
 
-let a = 5;
 
 
 const resultBlock = document.querySelector('#result')
 const pageNumber = document.querySelector('#page-number')
 const clickMeButton = document.querySelector('#click-me')
-clickMeButton.addEventListener('click', makeRequest)
+clickMeButton.addEventListener('click', ()=>{
+    getImages(pageNumber.value, onDataReceive);
+})
 
-function makeRequest(){
-    $.ajax(`https://repetitora.net/api/JS/Images?page=${pageNumber.value}&count=1`, {
-        success: function(data){
-            data.forEach(el=>{
-                const img = document.createElement('img');
-                img.src = el.thumbnail
-                document.querySelector('#result').appendChild(img)
-            })
-        }
-    });
+
+function onDataReceive(data){
+    data.forEach(el=>{
+        const img = document.createElement('img');
+        img.src = el.thumbnail
+        document.querySelector('#result').appendChild(img)
+    })
 }
-
-
-a = 8;
