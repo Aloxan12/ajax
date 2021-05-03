083f947ca128579f39a1692f4408c2936cbdc7de
Aloxan12 <kvakvaaak@mail.ru>
2021-05-03 15:49:09 +0300

    get param

--- main.js
+++ main.js
@@ -1,13 +1,23 @@
 
 let a = 5;
 
-$.ajax('https://repetitora.net/api/JS/Images', {
-    success: function(data){
-        data.forEach(el=>{
-            const img = document.createElement('img');
-            img.src = el.thumbnail
-            document.querySelector('body').appendChild(img)
-        })
-    }
-});
+
+const resultBlock = document.querySelector('#result')
+const pageNumber = document.querySelector('#page-number')
+const clickMeButton = document.querySelector('#click-me')
+clickMeButton.addEventListener('click', makeRequest)
+
+function makeRequest(){
+    $.ajax(`https://repetitora.net/api/JS/Images?page=${pageNumber.value}&count=1`, {
+        success: function(data){
+            data.forEach(el=>{
+                const img = document.createElement('img');
+                img.src = el.thumbnail
+                document.querySelector('#result').appendChild(img)
+            })
+        }
+    });
+}
+
+
 a = 8;
